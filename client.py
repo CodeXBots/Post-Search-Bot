9e456ce4d60e6bc05aaaaa7963b8ec4d38186bff
Code X Bots <125588667+codexbots@users.noreply.github.com>
2024-08-11 23:56:38 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,25 +1,16 @@
 from info import *
 from pyrogram import Client
-from subprocess import Popen
-
-User = Client(name="user", session_string=SESSION)
-DlBot = Client(name="auto-delete", 
-               api_id=API_ID,
-               api_hash=API_HASH,           
-               bot_token=BOT_TOKEN)
 
 class Bot(Client):   
     def __init__(self):
         super().__init__(   
-           "bot",
+           "vj-post-search-bot",
             api_id=API_ID,
             api_hash=API_HASH,           
             bot_token=BOT_TOKEN,
             plugins={"root": "plugins"})
     async def start(self):                        
-        await super().start()        
-        await User.start()
-        Popen("python3 -m utils.delete", shell=True)       
-        print("⚡ Bot Started ⚡")   
+        await super().start()  
+        print("Bot Started. By @CodeXBots")   
     async def stop(self, *args):
         await super().stop()

8391c89b55457ca6423922b43e2d892d78ce7d06
Code X Bots <125588667+codexbots@users.noreply.github.com>
2024-08-11 23:54:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,10 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  =
-ADMIN        = 1255023013
-CHANNEL      = "RahulReviews"
+import os
+from os import environ
+
+API_ID       = int(environ.get("API_ID", ""))
+API_HASH     = environ.get("API_HASH", "")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "")
+DATABASE_URI = environ.get("DATABASE_URI", "")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
+ADMIN        = int(environ.get("ADMIN", ""))
+CHANNEL      = environ.get("CHANNEL", "@RahulReviews")

b0c687ec90cbf191405452920f09c988460488e2
Code X Bots <125588667+codexbots@users.noreply.github.com>
2024-08-12 00:24:23 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -1,8 +1,9 @@
 class script(object):
-    START = """**{},**
+    START = """{},
 
-ɪ  ᴄᴀɴ  ᴘʀᴏᴠɪᴅᴇ  ᴍᴏᴠɪᴇs  ᴀɴᴅ  sᴇʀɪᴇs,
-ᴊᴜsᴛ  ᴀᴅᴅ  ᴍᴇ  ᴛᴏ  ʏᴏᴜʀ  ɢʀᴏᴜᴘ  ᴀɴᴅ  ᴇɴᴊᴏʏ  😍
+ɪ ᴀᴍ ᴘᴏsᴛ sᴇᴀʀᴄʜ ʙᴏᴛ,
+
+ɪ ᴡɪʟʟ ꜰɪʟᴛᴇʀ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴘᴏsᴛs ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴀɴᴅ sᴇɴᴅ ɪᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡʜᴇɴ sᴏᴍᴇᴏɴᴇ sᴇᴀʀᴄʜ ɪᴛ.
 
 <b>ꜱᴇɴᴅ /help ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏ</b>"""
 
@@ -16,35 +17,27 @@ class script(object):
 ❂ ᴡᴀɪᴛ ᴜɴᴛɪʟ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛ ɪꜱ ᴠᴇʀɪꜰɪᴇᴅ ʙʏ <b><a href="https://telegram.me/CodeXBro">ᴏᴡɴᴇʀ</a></b>
 
 ❂ ᴀꜰᴛᴇʀ ᴠᴇʀɪꜰɪᴄᴀᴛɪᴏɴ ꜱᴇɴᴅ /connect ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ɪᴅ
-
 <b>⤨ ᴇxᴀᴍᴘʟᴇ -</b>
 
-/connect -100xxxxxxxxxx
+/connect -100xxxxxxxxxxx
 
+❂ ʀᴇᴍᴏᴠᴇ ᴀ ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ
+/disconnect -100xxxxxxxxxxx
+ᴛʜɪꜱ ᴡɪʟʟ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ʀᴇᴍᴏᴠᴇ ᴀ ɪɴᴅᴇxᴇᴅ ᴄʜᴀɴɴᴇʟ ꜰʀᴏᴍ ʏᴏᴜʀ ɢʀᴏᴜᴘ.
 
 ❂ ꜰᴏʀ ᴀᴅᴅɪɴɢ ꜰᴏʀᴄᴇ ꜱᴜʙ ɪɴ ɢʀᴏᴜᴘ ᴛʏᴘᴇ /fsub ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ɪᴅ
-
 <b>⤨ ᴇxᴀᴍᴘʟᴇ -</b>
 
 /fub -100xxxxxxxxxx
 
-
-❂ ꜰᴏʀ ʀᴇᴍᴏᴠɪɴɢ ꜰᴏʀᴄᴇ ꜱᴜʙ ɪɴ ɢʀᴏᴜᴘ ᴛʏᴘᴇ /nofsub ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ɪᴅ
-
-<b>⤨ ᴇxᴀᴍᴘʟᴇ -</b>
-
-/nofsub -100xxxxxxxxxx
-
-
-⤨ ʀᴇᴍᴏᴠᴇ ᴀ ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ
-/disconnect -100xxxxxxxxxxx
-ᴛʜɪꜱ ᴡɪʟʟ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ʀᴇᴍᴏᴠᴇ ᴀ ɪɴᴅᴇxᴇᴅ ᴄʜᴀɴɴᴇʟ ꜰʀᴏᴍ ʏᴏᴜʀ ɢʀᴏᴜᴘ.
+❂ ꜰᴏʀ ʀᴇᴍᴏᴠɪɴɢ ꜰᴏʀᴄᴇ ꜱᴜʙ ɪɴ ɢʀᴏᴜᴘ ᴛʏᴘᴇ /nofsub
 
 ❂ ɢᴇᴛ ᴄᴏɴɴᴇᴄᴛᴇᴅ ᴄʜᴀɴɴᴇʟꜱ ʟɪꜱᴛ ᴡɪᴛʜ
 /connections"""
 
     ABOUT = """<b>➣ ᴍʏ ɴᴀᴍᴇ ⋟  {}</b>
 <b>➢ ᴄʀᴇᴀᴛᴏʀ ⋟  <a href=https://telegram.me/CodeXBro>ʀᴀʜᴜʟ</a></b>
+<b>➢ ᴄʜᴀɴɴᴇʟ  ⋟  <a href=https://youtube.com/@RahulReviews>ʀᴀʜᴜʟ ʀᴇᴠɪᴇᴡꜱ</a></b>
 <b>➢ ʟᴀɴɢᴜᴀɢᴇ ⋟  <a href=https://www.python.org>ᴘʏᴛʜᴏɴ 3</a></b>
 <b>➣ ᴅᴀᴛᴀʙᴀsᴇ ⋟  <a href=https://www.mongodb.com>ᴍᴏɴɢᴏ ᴅʙ</a></b>
 <b>➢ ʙᴏᴛ sᴇʀᴠᴇʀ ⋟  <a href=https://heroku.com>ʜᴇʀᴏᴋᴜ</a></b>
@@ -57,7 +50,7 @@ class script(object):
 
     BROADCAST = """<u>{}</u>
 
-Total: `{}`
-Remaining: `{}`
-Success: `{}`
-Failed: `{}`"""
+Total: {}
+Remaining: {}
+Success: {}
+Failed: {}"""

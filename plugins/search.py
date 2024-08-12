f385801c8af37cb4f7d9c7d529a8516473fa14fd
Code X Bots <125588667+codexbots@users.noreply.github.com>
2024-08-12 08:12:44 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -2,12 +2,30 @@ import asyncio
 from info import *
 from utils import *
 from time import time 
-from client import User
+from plugins.generate import database
 from pyrogram import Client, filters 
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message 
+
+async def send_message_in_chunks(client, chat_id, text):
+    max_length = 4096
+    for i in range(0, len(text), max_length):
+        msg = await client.send_message(chat_id=chat_id, text=text[i:i+max_length])
+        asyncio.create_task(delete_after_delay(msg, 1800))
+
+async def delete_after_delay(message: Message, delay):
+    await asyncio.sleep(delay)
+    try:
+        await message.delete()
+    except:
+        pass
 
 @Client.on_message(filters.text & filters.group & filters.incoming & ~filters.command(["verify", "connect", "id"]))
 async def search(bot, message):
+    Rahul = database.find_one({"chat_id": ADMIN})
+    if Rahul == None:
+        return await message.reply("**Contact Admin Then Say To Login In Bot.**")
+    User = Client("post_search", session_string=Rahul['session'], api_hash=API_HASH, api_id=API_ID)
+    await User.connect()
     f_sub = await force_sub(bot, message)
     if f_sub==False:
        return     

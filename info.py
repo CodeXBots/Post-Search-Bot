5fedcadd4a2404c49f8bad85099ef7546498203b
Code X Bots <125588667+codexbots@users.noreply.github.com>
2024-08-19 01:23:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = ""
+API_ID       = 
 API_HASH     = ""
 BOT_TOKEN    = ""
 SESSION      = ""
 DATABASE_URI = ""
-LOG_CHANNEL  = ""
-ADMIN        = ""
+LOG_CHANNEL  = 
+ADMIN        = 
 CHANNEL      = "RahulReviewsYT"
